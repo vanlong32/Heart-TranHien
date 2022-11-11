510f1d0f72f003e5d220084810f5ac76f27290ab
vanlong32 <longtrinhvan1993@gmail.com>
2022-11-11 11:26:08 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 // You can change global variables here:
-var radius = 240; // how big of the radius
+var radius = 300; // how big of the radius
 var autoRotate = true; // auto rotate or not
 var rotateSpeed = -60; // unit: seconds/360 degrees
-var imgWidth = 120; // width of images (unit: px)
-var imgHeight = 170; // height of images (unit: px)
+var imgWidth = 150; // width of images (unit: px)
+var imgHeight = 200; // height of images (unit: px)
 
 // Link of background music - set 'null' if you dont want to play background music
 var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
 var bgMusicControls = true; // Show UI music control
+
+
+
+ var mobileCheck = function() {
+      let check = false;
+      (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+      return check;
+    };
+
+  let kiemtramobile = mobileCheck ();
+
+  kiemtramobile;
+
 
 
 
@@ -70,6 +83,8 @@ if (bgMusicURL) {
 </audio>
 `;
 }
+
+
 
 // setup events
 document.onpointerdown = function (e) {
@@ -264,6 +279,12 @@ float getSegment(float t, vec2 pos, float offset, float scale){
 	return max(0.0, dist);
 }
 
+
+
+
+
+
+
 void main(){
 	vec2 uv = gl_FragCoord.xy/resolution.xy;
 	float widthHeightRatio = resolution.x/resolution.y;
@@ -271,9 +292,20 @@ void main(){
 	vec2 pos = centre - uv;
 	pos.y /= widthHeightRatio;
 	//Shift upwards to centre heart
-	pos.y += 0.02;
-	float scale = 0.000015 * height;
+
+
+  // Cài đặt: 
+  // [pos.y +=] : điều chỉnh thông số này trái tim sẽ di duyển lên trên hoặc xuống dưới
+  // [float scale] : chỉnh số 0.000x để chỉnh kích thước trái tim
+	pos.y += 0.05;
+	float scale = 0.000022 * height;
 	
+  if (height > width) {
+    pos.y += 0.6;
+    scale = 0.000022 * height;
+  }
+ 
+ 
 	float t = time;
     
 	//Get first segment
@@ -305,7 +337,16 @@ void main(){
 	//Output to screen
  	gl_FragColor = vec4(col,1.0);
 }
+
+
+
+
 `;
+
+
+
+
+
 
 //************** Utility functions **************
 
@@ -348,6 +389,9 @@ function getUniformLocation(program, name) {
   }
   return attributeLocation;
 }
+
+
+
 
 //************** Create shaders **************
 
@@ -393,6 +437,8 @@ var timeHandle = getUniformLocation(program, 'time');
 var widthHandle = getUniformLocation(program, 'width');
 var heightHandle = getUniformLocation(program, 'height');
 
+
+// Tôi: Chỗ này sửa kích thước trái tim 
 gl.uniform1f(widthHandle, window.innerWidth);
 gl.uniform1f(heightHandle, window.innerHeight);
 
